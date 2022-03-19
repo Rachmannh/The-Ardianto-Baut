48a925ad117f901886cbe1d5b54f06ad2eeb3820
Rachman Nul Hakim <69642186+Rachmannh@users.noreply.github.com>
2022-03-19 23:39:11 +0700

    Add files via upload

---
+++ faq.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="./assets/img/logo.png">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="./assets/css/main.css">
+  <link rel="stylesheet" href="./assets/css/responsive.css">
+
+  <!-- Animation -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+
+  <!-- Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+  <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+  <!-- Font -->
+  <link
+    href="https://fonts.googleapis.com/css2?family=Catamaran:wght@300;400;500&family=Hind+Madurai:wght@300;400&display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Habibi&family=Hind+Madurai:wght@300;400&display=swap"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300;400&family=Martel+Sans:wght@300;400;600;700&display=swap"
+    rel="stylesheet">
+
+  <!-- Icon -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+
+  <title>Ido Baut</title>
+  </head>
+
+  <body>
+      
+      <!-- Navbar -->
+   <?php
+   include 'navbar.php';
+   ?>
+   <!-- Akhir Navbar -->
+ 
+   <!-- Back to top -->
+   <button onclick="topFunction()" id="myBtn" title="Go to top">
+     <ion-icon name="caret-up-outline"></ion-icon>
+   </button>
+   <!-- Akhir back to top -->
+
+   <!-- Content -->
+   <section class="faq">
+       <div class="container">
+           <h1 class="title">
+               Frequently Ask Questions
+           </h1>
+<div class="row gx-3 gx-xxl-5 mt-5">
+  <div class="col">
+    <div class="p-3">
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+
+    <!-- Accordion 1 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Jasa Pengiriman
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt similique ad autem beatae ex quaerat eum consequuntur quod itaque omnis non harum et optio laudantium dignissimos, sit deleniti ut iusto?</div>
+    </div>
+  </div>
+  <!-- End -->
+
+  <!-- Accordion 2 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Bagaimana cara memesan barang pada Ido Baut ?
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, laborum accusamus beatae, cum illum quos sed culpa porro quibusdam cumque laudantium dolores, possimus molestiae animi dolorum sunt eaque magni veniam voluptates nemo! Ipsum molestias doloribus aspernatur autem aperiam quaerat sit ex, assumenda sint corporis beatae aliquid sequi, omnis dicta tenetur.</div>
+    </div>
+  </div>
+  <!-- End -->
+
+  <!-- Accordion 3 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Lorem
+      </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos esse mollitia reiciendis quod nisi, accusamus hic sequi vero tempore, non provident dolores perspiciatis. Quod quas asperiores, expedita non similique deserunt, cum odit voluptatibus, maiores perspiciatis quasi itaque exercitationem incidunt officia?</div>
+    </div>
+  </div>
+  <!-- End -->
+
+  <!-- Accordion 4 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+        Lorem Ipsum Lorem Lorem
+      </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos esse mollitia reiciendis quod nisi, accusamus hic sequi vero tempore, non provident dolores perspiciatis. Quod quas asperiores, expedita non similique deserunt, cum odit voluptatibus, maiores perspiciatis quasi itaque exercitationem incidunt officia?</div>
+    </div>
+  </div>
+  <!-- End -->
+
+  <!-- Accordion 5 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFive">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+        Lorem Ipsum Lorem Lorem
+      </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos esse mollitia reiciendis quod nisi, accusamus hic sequi vero tempore, non provident dolores perspiciatis. Quod quas asperiores, expedita non similique deserunt, cum odit voluptatibus, maiores perspiciatis quasi itaque exercitationem incidunt officia?</div>
+    </div>
+  </div>
+  <!-- End -->
+
+  <!-- Accordion 6 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header bg-danger" id="flush-headingSix">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+        Lorem Ipsum Lorem Lorem
+      </button>
+    </h2>
+    <div id="flush-collapseSix" class="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos esse mollitia reiciendis quod nisi, accusamus hic sequi vero tempore, non provident dolores perspiciatis. Quod quas asperiores, expedita non similique deserunt, cum odit voluptatibus, maiores perspiciatis quasi itaque exercitationem incidunt officia?</div>
+    </div>
+  </div>
+  <!-- End -->
+
+  <!-- Accordion 7 -->
+  <div class="accordion-item">
+    <h2 class="accordion-header bg-danger" id="flush-headingSeven">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+        Lorem Ipsum Lorem Lorem
+      </button>
+    </h2>
+    <div id="flush-collapseSeven" class="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos esse mollitia reiciendis quod nisi, accusamus hic sequi vero tempore, non provident dolores perspiciatis. Quod quas asperiores, expedita non similique deserunt, cum odit voluptatibus, maiores perspiciatis quasi itaque exercitationem incidunt officia?</div>
+    </div>
+  </div>
+  <!-- End -->
+
+</div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="p-3">
+    <img src="./assets/img/faq.png" class="faq-images img-fluid" alt="FaQ Illustration">
+    </div>
+  </div>
+</div>
+       </div>
+   </section>
+   <!-- Akhir Content -->
+
+     <!-- Footer -->
+  <div class="container-footer row mt-5">
+      <?php
+      include 'footer.php';
+      ?>
+    </div>
+    <!-- Akhir Footer -->
+
+   <!-- Option 2: Separate Popper and Bootstrap JS -->
+
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+  </body>
+</html>
