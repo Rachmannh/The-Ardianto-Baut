aa788b45fcdb5b2531bd983bdf6c59d1eacfb510
Your Name <yudhaxsinatra@gmail.com>
2022-06-05 14:44:15 +0700

    Update Navbar Admin + Responsive

--- assets/function/index.php
+++ assets/function/index.php
@@ -17,7 +17,7 @@ include 'template/header.php';
 <!-- Content -->
 <div class="info-index text-center position-absolute top-50 start-50 translate-middle">
     <h3 class="pt-3 pb-2 ms-5 ps-5">Dashboard Admin</h3>
-    <h5 class="pb-1 ms-5 ps-5">Wellcome Admin</h5>
+    <h5 class="pb-1">Wellcome Admin</h5>
 </div>
 <!-- End Content -->
 
--- assets/function/template/header.php
+++ assets/function/template/header.php
@@ -9,32 +9,76 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <!-- css.gg -->
-<link href='https://css.gg/css' rel='stylesheet'>
+    <link href='https://css.gg/css' rel='stylesheet'>
 
-<!-- UNPKG -->
-<link href='https://unpkg.com/css.gg/icons/all.css' rel='stylesheet'>
+    <!-- UNPKG -->
+    <link href='https://unpkg.com/css.gg/icons/all.css' rel='stylesheet'>
 
-<!-- JSDelivr -->
-<link href='https://cdn.jsdelivr.net/npm/css.gg/icons/all.css' rel='stylesheet'>
+    <!-- JSDelivr -->
+    <link href='https://cdn.jsdelivr.net/npm/css.gg/icons/all.css' rel='stylesheet'>
+
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 
-<body>
-    <div class="fluid-container">
-        <div class="row">
-            <div class="col-2">
-                <!-- Navbar -->
-                <nav class="nav-side">
-                    <a href="index.php" class="title">
-                        <h2>Ido Baut</h2>
-                    </a>
-                    <ul class="nav-list">
-                        <li><a href="index.php"><i class="bi bi-house icon"></i> Home</a></li>
-                        <li><a href="tbl_data_barang.php"><i class="bi bi-nut icon"></i> Data Barang</a></li>
-                        <li><a href="tbl_barang_masuk.php"><i class="bi bi-inboxes icon"></i> Input Barang</a></li>
-                        <li><a href="tbl_barang_keluar.php"><i class="bi bi-box-seam icon"></i> Barang Keluar</a></li>
-                        <li><a href="logout.php"><i class="bi bi-box-arrow-right icon"></i> Log Out</a></li>
-                    </ul>
-                </nav>
-                <!-- End Navbar -->
+<body id="body-pd">
+    <header class="header" id="header">
+        <div class="header_toggle"> <i class="bx bx-menu" id="header-toggle"></i> </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div>
+                <a href="index.php" class="nav_logo">
+                    <i class='bx bxs-store nav_logo-icon'></i>
+                    <span class="nav_logo-name">The Ardianto Baut</span>
+                </a>
+                <div class="nav_list">
+                    <li class="nav_link">
+                        <a href="index.php">
+                            <i class='bx bx-home-alt-2 nav_icon'></i>
+                            <span class="nav_name">
+                                Home
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav_link">
+                        <a class="logo-side" href="tbl_data_barang.php">
+                            <i class='bx bx-table nav_icon'></i>
+                            <span class="nav_name">
+                                Data Barang
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav_link">
+                        <a class="logo-side" href="tbl_barang_masuk.php">
+                            <i class='bx bx-archive-in nav_icon'></i>
+                            <span class="nav_name">
+                                Input Barang
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav_link">
+                        <a class="logo-side" href="tbl_barang_keluar.php">
+                            <i class='bx bx-package nav_icon'></i>
+                            <span class="nav_name">
+                                Barang Keluar
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav_link">
+                        <a class="logo-side" href="logout.php">
+                            <i class='bx bx-log-out nav_icon'></i>
+                            <span class="nav_name">
+                                Log Out
+                            </span>
+                        </a>
+                    </li>
+                </div>
             </div>
+        </nav>
+    </div>
+    <!--Container Main start-->
